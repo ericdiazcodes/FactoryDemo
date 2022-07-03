d7e67c2c879ce0371fb77e1315cc04db15874c9c
Eric <ericdiazjr21@gmail.com>
2022-07-03 18:34:22 -0400

    rename factory

--- src/variationkotlin/BetterCarFactory.kt
+++ src/variationkotlin/BetterCarFactory.kt
@@ -2,24 +2,24 @@ package variationkotlin
 
 // Define an interface that declares what object it will create. Since it's
 // an interface, how the Car will be created will be defined by the subclass
-interface ProperCarFactory {
+interface BetterCarFactory {
     fun getCar(): Car
 }
 
 // Create subclasses of the Factory that will return their own specific subclass of Car.
-class HondaMotorFactory : ProperCarFactory {
+class HondaMotorFactory : BetterCarFactory {
     override fun getCar(): Car = Accord()
 }
 
-class BMWGroupFactory : ProperCarFactory {
+class BMWGroupFactory : BetterCarFactory {
     override fun getCar(): Car = BMW()
 }
 
-class FordMotorFactory : ProperCarFactory {
+class FordMotorFactory : BetterCarFactory {
     override fun getCar(): Car = Mustang()
 }
 
-class GeneralMotorsFactory : ProperCarFactory {
+class GeneralMotorsFactory : BetterCarFactory {
     override fun getCar(): Car = Cadillac()
 }
 
